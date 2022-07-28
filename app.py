c033ee97aaee25b6130e4c3d52e0bf4d1ce70459
michael <michael12robinsonxd@gmail.com>
2022-07-29 08:51:10 +1000

    added css and js

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config.from_object(Config)  # loads the configuration for the database
+app.config.from_object(Config)  # loads the configuration for the ngunnawal.db
 db = SQLAlchemy(app)            # creates the db object using the configuration
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def homepage():  # put application's code here
+    return render_template("index.html", title="Ngunnawal Country")
 
 
 if __name__ == '__main__':
