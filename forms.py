a5561dbeed0e237d87d2d2f1fa21117d4e6de772
michael <michael12robinsonxd@gmail.com>
2022-10-29 16:45:13 +1100

    errors

--- forms.py
+++ forms.py
@@ -1,9 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired, Email
-from wtforms import StringField, SubmitField, IntegerField
-
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from wtforms import StringField, SubmitField, IntegerField, PasswordField
+from models import UserMixin
 class ContactForm (FlaskForm):
     name = StringField("name", validators=[DataRequired()])
     email = StringField("email", validators=[DataRequired(), Email()])
     message = StringField("message", validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+    class RegistrationForm(FlaskForm):
+        email_address = StringField("Email Address (Username)", validators=[DataRequired(), Email()])
+        name = StringField("Full Name", validators=[DataRequired()])
+        password = PasswordField("Password", validators=[DataRequired()])
+        password_confirm = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+        submit = SubmitField("Register")
+
+        def validate_email_address(self, email_address_to_register):
+            user = UserMixin.query.filter_by(email_address=email_address_to_register.data).first()
+            if user is not None:
+                raise ValidationError("Please Use a Different Email Address)")
+
+    class LoginForm(FlaskForm):
+        email_address = StringField('Email Address', validators=[DataRequired()])
+        password = PasswordField('Password', validators=[DataRequired()])
+        submit = SubmitField('Sign In')
+
+    class ResetPasswordForm(FlaskForm):
+        new_password = StringField('New Password', validators=[DataRequired()])
+        submit = SubmitField('Submit')
+
+        class UserProfileForm(FlaskForm):
+            email_address = StringField("Email Address (Username)", validators=[DataRequired(), Email()])
+            name = StringField("Full Name", validators=[DataRequired()])
+            submit = SubmitField("Update Profile")
+
+
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -13,8 +15,36 @@ class Contact(db.Model):
         self.email = email
         self.message = message
         self.dateSubmitted = datetime.today()
-        
+
+
 class todo(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.Text)
     done = db.Column(db.Boolean)
+
+    class User(UserMixin, db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        email_address = db.Column(db.String(255), unique=True)
+        name = db.Column(db.String(255))
+        password_hash = db.Column(db.String(255))
+        user_level = db.Column(db.Integer)
+
+        def set_password(self, password):
+            self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def update_details(self, email_address, name):
+        self.email_address = email_address
+        self.name = name
+
+    def is_admin(self):
+        if self.user_level == 2:
+            return True
+        else:
+            return False
+
+        @login.user_loader
+        def load_user(id):
+            return UserMixin.query.get(int(id))
